d5dc531e4d48dfcf7f5689df16d9caef17eff104
penaone <penaone@gmail.com>
2020-08-14 21:51:39 -0700

    player name bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,25 @@
 //    * Defeat each enemy robot
 // "LOSE" - Player robot's health is zero or less
 
+
+// function to set name
+
+var getPlayerName = function() {
+  var name = "";
+  
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  
+  console.log("","Your robot's name is", "" + name);
+  return name;
+
+
+};
+/* GAME INFORMATION / VARIABLES */
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
+  // other playerInfo properties and methods
   health: 100,
   attack: 10,
   money: 10,
@@ -126,6 +143,7 @@ var startGame = function () {
     if (playerInfo.health > 0) {
       // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert("Welcome to Battlebots! Round " + (i + 1));
+      
 
       // pick new enemy to fight based on the index of the enemyNames array    
       var pickedEnemyObj = enemyInfo[i];
